9c8c8b674a60b60a10b3b835fc1e0c9c882b4258
Warren O'Connell <Warren-16@live.com>
2015-09-12 22:56:57 -0400

    msg

--- tester/codelink.php
+++ tester/codelink.php
@@ -27,25 +27,38 @@
 		// return json_decode($result);//return $result
 		$result = '';
 		
-
+		//get current working directory
 		$loc=shell_exec("echo \$PWD");
+
+		//$PWD returns extra space at end, this removes it
 		$loca = substr($loc, 0, -1);
+
+		//adds relative location of jsons to create absolute path
 		$loc = $loca."/jsons/";
+
+		//get current microsecond of each second and converts it to integer
 		$micro = microtime()*1000000;
+
+		//gets time in seconds and append micro seconds for higher precision
 		$time = time(). "" .$micro;
+
+		//create variables for file names
 		$inFileName = $time."-".$dataname."-input.json";
 		$outFileName = $time."-".$dataname."-output.json";
+
+		//open file, write data, close file
 		$myfile = fopen($loc.$inFileName, "w");
 		fwrite($myfile,json_encode($data));
 		fclose($myfile);
-		//file_put_contents($inFileName, json_encode($data));
 
-		echo "loc: ".$loc."<br>";
-		echo $inFileName."<br>";
-		echo $outFileName."<br>";
-		//$dataname=$program;
+		//debug code
+		//echo "loc: ".$loc."<br>";
+		//echo $inFileName."<br>";
+		//echo $outFileName."<br>";
 
+		//call program
 		shell_exec('python '.$program.' '.$dataname. " ". $time . " " . $loc);
+		
 		echo 'done';
 	}
 
